a2e4049fff23a8bd4bab2ff6ece376c440f6a1f6
ramiro2021 <ramiroamurua@hotmail.com>
2020-10-07 09:15:14 -0300

    feat: Landing Page - Benefits

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  benefits: Benefits[] = [
+    {
+      name: 'Flexibilidad Horaria',
+      image: './assets/Ic_Hour.svg'
+    },
+    {
+      name: 'Home Office',
+      image: './assets/Ic_HomeOffice.svg'
+    },
+    {
+      name: 'Capacitaciones y workshops',
+      image: './assets/Ic_Workshops.svg'
+    },
+    {
+      name: 'Snacks, frutas y bebidas gratis',
+      image: './assets/Ic_DrinkSnacks.svg'
+    },
+    {
+      name: 'Semana Remota',
+      image: './assets/Ic_laptop.svg'
+    },
+    {
+      name: 'Trabajar en ultimas tecnologias',
+      image: './assets/Ic_brain.svg'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Benefits {
+  name: string;
+  image: string;
 }
